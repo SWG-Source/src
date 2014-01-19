ce49bfcfaa6c1d67230c2c35eadf845daa4ea4b4
Anonymous <anonymous@anonymous.com>
2014-01-19 02:55:26 -0700

    Not needed in newer versions of linux

--- engine/server/library/serverUtility/src/linux/stlhack.cpp
+++ engine/server/library/serverUtility/src/linux/stlhack.cpp
@@ -6,17 +6,18 @@
 // being explicitly defined.  So we have to instantiate one.
 // We should try removing this file when either we a) upgrade the compiler or b) upgrate the OS.
 
-#define __stl_num_primes 28
-#define __PRIME_LIST_BODY { \
-  53ul,         97ul,         193ul,       389ul,       769ul,      \
-  1543ul,       3079ul,       6151ul,      12289ul,     24593ul,    \
-  49157ul,      98317ul,      196613ul,    393241ul,    786433ul,   \
-  1572869ul,    3145739ul,    6291469ul,   12582917ul,  25165843ul, \
-  50331653ul,   100663319ul,  201326611ul, 402653189ul, 805306457ul,\
-  1610612741ul, 3221225473ul, 4294967291ul  \
-}
-
-template <> const size_t std::_Stl_prime<bool>::_M_list[__stl_num_primes] = __PRIME_LIST_BODY;
-
-#undef __stl_num_primes
-#undef __PRIME_LIST_BODY
+//#define __stl_num_primes 28
+//#define __PRIME_LIST_BODY { \
+//  53ul,         97ul,         193ul,       389ul,       769ul,      \
+//  1543ul,       3079ul,       6151ul,      12289ul,     24593ul,    \
+//  49157ul,      98317ul,      196613ul,    393241ul,    786433ul,   \
+//  1572869ul,    3145739ul,    6291469ul,   12582917ul,  25165843ul, \
+//  50331653ul,   100663319ul,  201326611ul, 402653189ul, 805306457ul,\
+//  1610612741ul, 3221225473ul, 4294967291ul  \
+//}
+//
+//template <> const size_t std::_Stl_prime<bool>::_M_list[__stl_num_primes] = __PRIME_LIST_BODY;
+//
+//#undef __stl_num_primes
+//#undef __PRIME_LIST_BODY
+//
