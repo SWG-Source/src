154aa76d66232f775bdc8d4970b146f8b3087370
DarthArgus <asdf@asdf.com>
2014-10-27 20:25:08 -0700

    revert the CS stuff as we don't care

--- external/3rd/library/soePlatform/CSAssist/projects/CSAssist/CSAssistgameapi/request.h
+++ external/3rd/library/soePlatform/CSAssist/projects/CSAssist/CSAssistgameapi/request.h
@@ -96,7 +96,7 @@ class RConnectCSAssist : public Request
 //----------------------------------------------
 {
 	public:
-		RConnectCSAssist(unsigned track, const Plat_Unicode::String GameName, const Plat_Unicode::String &ServerName);
+		RConnectCSAssist(unsigned track, const Plat_Unicode::String GameName, const Plat_Unicode::String ServerName);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		Plat_Unicode::String mGameName;
@@ -117,7 +117,7 @@ class RNewTicketActivity : public Request
 //----------------------------------------------
 {
 	public:
-		RNewTicketActivity(unsigned track, const unsigned uid, const Plat_Unicode::String &character);
+		RNewTicketActivity(unsigned track, const unsigned uid, const Plat_Unicode::String character);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned			 mUID;
@@ -129,7 +129,7 @@ class RRegisterCharacter : public Request
 //----------------------------------------------
 {
 	public:
-		RRegisterCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String &character, const unsigned avaconID);
+		RRegisterCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String character, const unsigned avaconID);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned				mUID;
@@ -142,7 +142,7 @@ class RUnRegisterCharacter : public Request
 //----------------------------------------------
 {
 	public:
-		RUnRegisterCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String &character);
+		RUnRegisterCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String character);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned			  mUID;
@@ -154,7 +154,7 @@ class RGetIssueHierarchy : public Request
 //----------------------------------------------
 {
 	public:
-		RGetIssueHierarchy(unsigned track, const Plat_Unicode::String &version, const Plat_Unicode::String &language);
+		RGetIssueHierarchy(unsigned track, const Plat_Unicode::String version, const Plat_Unicode::String language);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		Plat_Unicode::String mVersion;
@@ -166,7 +166,7 @@ class RCreateTicket : public Request
 //----------------------------------------------
 {
 	public:
-		RCreateTicket(unsigned track, const CSAssistGameAPITicket *ticketBody, const Plat_Unicode::String &XMLBody, const unsigned uid);
+		RCreateTicket(unsigned track, const CSAssistGameAPITicket *ticketBody, const Plat_Unicode::String XMLBody, const unsigned uid);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		CSAssistGameAPITicket	mTicketStruct;
@@ -179,7 +179,7 @@ class RAppendComment : public Request
 //----------------------------------------------
 {
 	public:
-		RAppendComment(unsigned track, const unsigned ticket, const unsigned uid, const Plat_Unicode::String &character, const Plat_Unicode::String &comment);
+		RAppendComment(unsigned track, const unsigned ticket, const unsigned uid, const Plat_Unicode::String character, const Plat_Unicode::String comment);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned			 mTicketID;
@@ -219,7 +219,7 @@ class RGetTicketByCharacter : public Request
 //----------------------------------------------
 {
 	public:
-		RGetTicketByCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String &character, const unsigned Start, const unsigned Count, const unsigned MarkAsRead);
+		RGetTicketByCharacter(unsigned track, const unsigned uid, const Plat_Unicode::String character, const unsigned Start, const unsigned Count, const unsigned MarkAsRead);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned				mUID;
@@ -245,7 +245,7 @@ class RCancelTicket : public Request
 //----------------------------------------------
 {
 	public:
-		RCancelTicket(unsigned track, const unsigned ticket, const unsigned uid, const Plat_Unicode::String &comment);
+		RCancelTicket(unsigned track, const unsigned ticket, const unsigned uid, const Plat_Unicode::String comment);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned				mTicketID;
@@ -269,7 +269,7 @@ class RReplyGameLocation : public Request
 //----------------------------------------------
 {
 	public:
-		RReplyGameLocation(unsigned track, const unsigned sourceTrack, const unsigned uid, const Plat_Unicode::String &character, const unsigned csruid, const Plat_Unicode::String locationString);
+		RReplyGameLocation(unsigned track, const unsigned sourceTrack, const unsigned uid, const Plat_Unicode::String character, const unsigned csruid, const Plat_Unicode::String locationString);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		unsigned				mSourceTrack;
@@ -318,7 +318,7 @@ class RGetKBArticle : public Request
 //----------------------------------------------
 {
 	public:
-		RGetKBArticle(unsigned track, const Plat_Unicode::String &id, const Plat_Unicode::String &language, const unsigned uid);
+		RGetKBArticle(unsigned track, const Plat_Unicode::String id, const Plat_Unicode::String language, const unsigned uid);
 		virtual void pack(Base::ByteStream &msg);
 	private:
 		Plat_Unicode::String mID;
@@ -331,10 +331,10 @@ class RSearchKB : public Request
 //----------------------------------------------
 {
 	public:
-		RSearchKB(unsigned track, const Plat_Unicode::String &searchstring, const Plat_Unicode::String &language, const unsigned uid);
+		RSearchKB(unsigned track, const Plat_Unicode::String searchstring, const Plat_Unicode::String language, const unsigned uid);
 		virtual void pack(Base::ByteStream &msg);
 	private:
-		Plat_Unicode::String mSearchstring;
+		Plat_Unicode::String mSearchString;
 		Plat_Unicode::String mLanguage;
 		unsigned mUID;
 };
@@ -344,10 +344,10 @@ class RConnectLB : public Request
 //----------------------------------------------
 {
 	public:
-		RConnectLB(unsigned track, const std::string &connectString);
+		RConnectLB(unsigned track, const std::string connectString);
 		virtual void pack(Base::ByteStream &msg);
 	private:
-		std::string mconnectString;
+		std::string mConnectString;
 };
 
 } // namespace CSAssist
