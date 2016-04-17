a76c5dd70b96d6dfb0fba5fb7ce4c7908523a1e2
DarthArgus <darg.us@yandex.com>
2016-04-17 19:17:41 +0000

    make things more standardized

--- engine/server/application/LoginServer/src/shared/ClientConnection.cpp
+++ engine/server/application/LoginServer/src/shared/ClientConnection.cpp
@@ -210,7 +210,7 @@ void ClientConnection::validateClient(const std::string & id, const std::string 
 			std::string password(curl_easy_escape(curl, key.c_str(), 0));
 			std::string ip(curl_easy_escape(curl, getRemoteAddress().c_str(), 0));
 
-			curl_easy_setopt(curl, CURLOPT_URL, (authURL + username + "&pw=" + password + "&ip=" + ip));
+			curl_easy_setopt(curl, CURLOPT_URL, (authURL + "?user_name=" + username + "&user_password=" + password + "&user_ip=" + ip));
 			curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
 			curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
 			
