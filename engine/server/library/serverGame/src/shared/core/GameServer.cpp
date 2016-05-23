7139c1354dd57dcc604a31ae9a515ff1a371c6ab
DarthArgus <darg.us@yandex.com>
2016-05-23 00:25:06 +0000

    log when we send a keepalive, it may be related to being overloaded in which case we may need to throttle some things a bit...

--- engine/server/library/serverGame/src/shared/core/GameServer.cpp
+++ engine/server/library/serverGame/src/shared/core/GameServer.cpp
@@ -888,6 +888,7 @@ void GameServer::receiveMessage(const MessageDispatch::Emitter & source, const M
 	{
 		if(Clock::timeMs() - s_lastTaskKeepaliveTime > 60000)
 		{
+			DEBUG_WARNING(true, ("Sending keepalive message to taskmanager for process %i", Os::getProcessID()));
 			static const GenericValueTypeMessage<unsigned long> gameServerTaskManagerKeepAlive("GameServerTaskManagerKeepAlive", Os::getProcessId());
 			getInstance().m_taskManagerConnection->send(gameServerTaskManagerKeepAlive, true);
 			s_lastTaskKeepaliveTime = Clock::timeMs();
